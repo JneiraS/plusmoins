32862d270be7c344057ba8b001eecda26cfb4176
NeiraJ <johnny.neira@gmail.com>
2024-09-06 12:15:49 +0200

    refactor: ajout de commentaires et amélioration du code pour une meilleure lisibilité
    - Ajout de commentaires explicatifs dans les fonctions pour clarifier le processus de calcul.
    - Amélioration de l'indentation et des espaces pour une meilleure structure du code.
    - Ajout de vérifications pour gérer les cas où la liste contient moins de deux éléments dans les fonctions de recherche des deuxièmes plus grands et plus petits nombres.
    - Légère refactorisation de la fonction `getRandomNumber` avec l'ajout de commentaires pour expliciter la génération de l'indice aléatoire.

--- randomNumberGen.js
+++ randomNumberGen.js
@@ -4,10 +4,16 @@
  * @returns {number} La moyenne des éléments dans la liste
  */
 function calculerMoyenne(liste) {
+  // Initialisation de la somme
   let somme = 0;
+
+  // Boucle qui parcourt la liste
   for (let i = 0; i < liste.length; i++) {
+    // Ajout de chaque élément à la somme
     somme += liste[i];
   }
+
+  // Calcul de la moyenne
   return somme / liste.length;
 }
 
@@ -19,23 +25,32 @@ function calculerMoyenne(liste) {
 function trouverDeuxiemeNombreLePlusGrand(liste) {
   // Trier la liste par ordre décroissant
   liste.sort((a, b) => b - a);
-  
 
+  // Si la liste contient moins de 2 éléments, retourner 100
   if (liste.length < 2) {
     return 100;
   } else {
 
+    // Retourner le deuxième élément de la liste
     return liste[1];
   }
 }
 
-
+/**
+ * Trouve le deuxième plus petit nombre dans une liste
+ * @param {Array<number>} liste - Une liste de nombres
+ * @returns {number} Le deuxième plus petit nombre de la liste, ou 1 si la liste contient moins de 2 éléments
+ */
 function trouverDeuxiemeNombreLePlusPetit(liste) {
+  // Trier la liste par ordre croissant
   liste.sort((a, b) => a - b);
-  
+
+  // Si la liste contient moins de 2 éléments, retourner 1
   if (liste.length < 2) {
     return 1;
   } else {
+
+    // Retourner le deuxième élément de la liste
     return liste[1];
   }
 }
@@ -47,9 +62,12 @@ function trouverDeuxiemeNombreLePlusPetit(liste) {
  * @returns {number} Un nombre aléatoire entre min et max
  */
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  // Calcul de l'indice aléatoire
+  const indice = Math.floor(Math.random() * (max - min + 1)) + min;
 
+  // Retour de l'indice aléatoire
+  return indice;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
   const proposalElement = document.getElementById("proposal");
@@ -99,3 +117,5 @@ document.addEventListener("DOMContentLoaded", function() {
     console.error("Un des éléments est manquant");
   }
 });
+
+
